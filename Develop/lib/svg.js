209a31d12cfa1d64091cb9c1d6128f258591fa15
Adam Colin <accolin89@gmail.com>
2023-06-14 10:57:28 -0500

    i'm not sure what I changed, but committing in case I lose something

--- Develop/lib/svg.js
+++ Develop/lib/svg.js
@@ -5,10 +5,10 @@ class SVG {
     }
 
     render() {
-        return (`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.text}${this.shape2}</svg>`)
+        return (`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shape2}${this.text}</svg>`)
     }
     setText(characters, text_color) {
-        this.text = `<text x="100" y="100" font-size="20" fill="${text_color}">${characters}</text>`
+        this.text = `<text x="150" y="100" font-size="20" text-anchor="middle" fill="${text_color}">${characters}</text>`
     }
     setShape(userShape) {
         this.shape2 = userShape.render()
