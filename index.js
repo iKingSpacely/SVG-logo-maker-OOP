289f546d9c92004efaa6d23a1ee85d0d9c6abbc2
Adam Colin <accolin89@gmail.com>
2023-06-13 19:04:46 -0500

    working through capturing the user input for each prompt

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Square, Triangle, Circle } = require("./lib/shapes.js");
+const { Square, Triangle, Circle } = require('./Develop/lib/shapes');
 
 inquirer
-    .prompt ([
+    .prompt([
         {
             type: 'input',
             message: 'Please enter up to Three characters',
-            name: 'text',
+            name: 'characters',
             validate: textInput => {
                 if (textInput.length <= 3) {
                     return true;
@@ -16,7 +16,7 @@ inquirer
         },
         {
             type: 'input',
-            message: 'Please input a color.',
+            message: 'Please input a color for the shape.',
             name: 'colors',
         },
         {
@@ -25,10 +25,35 @@ inquirer
             name: 'shape',
             choices: ['Square', 'Circle', 'Triangle'],
         },
+        {
+            type: 'input',
+            message: 'Please input a color for the text.',
+            name: 'text_color',
+        },
 
     ])
+    .then(({ characters, colors, shape, text_color }) => {createSVG(characters, colors, shape, text_color)
+
+    })
 
     // .then((file) => {
     //     const content = generateShapes(file)
     //     fs.writeFile("logos.svg", content, (error) => error ? console.log("Something went wrong!", error) : console.log("Logo file created successfully!"))
-    // });
+    // })
+
+function createSVG(characters, colors, shape, text_color) {
+    let userShape;
+    if (shape = 'Triangle') {
+        userShape = new Triangle();
+    } else if (shape = 'Square') {
+        userShape = new Square();
+    } else if (shape = 'Circle') {
+        userShape = new Circle();
+    } else {
+        alert('Please restart the prompts!');
+        return 0
+    } 
+    
+    // console.table({"characters": characters, "colors": colors, "shape": shape, "text_color": text_color})
+
+}
